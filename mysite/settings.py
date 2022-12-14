827420b6123ea819829f61a2b62fab33c0e0613f
kkookkss <pre0919@gmail.com>
2022-12-15 06:01:07 +0900

    allowed host update

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
